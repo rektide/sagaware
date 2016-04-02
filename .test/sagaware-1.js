da32e656453851174c2cc701273a1dfdb1da0e1b
rektide <rektide@voodoowarez.com>
2016-04-02 15:48:24 -0400

    Create a Sagaware top level object, as opposed to attaching to state.

--- .test/sagaware-1.js
+++ .test/sagaware-1.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+"use strict"
+
 var
   Immutable= require( "immutable"),
   Tape= require( "tape")
@@ -7,23 +10,32 @@ var
 Tape("Base use", function( t){
 	var
 	  expected1= {"hi": "ho"},
-	  state1= Sagaware( expected1)
-	t.ok( state1.saga, "state1 as a saga")
-	t.ok( Immutable.Iterable.isIterable(state1.saga), "state1 saga is iterable immutable")
-	t.deepEqual( state1.toJS(), expected1, "state1 starts equal")
-	t.deepEqual( state1.saga.get(0).toJS(), expected1, "state1 has a saga entry")
+	  saga1= Sagaware( expected1)
+	t.ok( saga1.saga, "saga1 as a saga")
+	t.ok( saga1.state, "saga1 has a state")
+	t.ok( Immutable.Iterable.isIterable(saga1.saga), "saga1 saga is iterable immutable")
+	t.deepEqual( saga1.state.toJS(), expected1, "saga1 starts equal")
+	t.deepEqual( saga1.saga.get(0).toJS(), expected1, "saga1 has a saga entry")
 
 	var
-	  state2= state1.set("toWork", "we go"),
+	  saga2= saga1.clone()
+	t.equal( saga2.saga.size, 1, "clone has one saga entry")
+	t.deepEqual( saga2.state.toJS(), expected1, "clone has state")
+	t.deepEqual( saga2.saga.get(0).toJS(), expected1, "clone has saga entry")
+
+     var
+	  state2= saga1.state.set( "toWork", "we go"),
 	  expected2= {
 	  	"hi": "ho",
 	  	"toWork": "we go"}
-	state1.putState(state2)
-	t.ok( state2.saga, "state2 has a saga")
-	t.ok( Immutable.Iterable.isIterable(state2.saga), "state2 aga is iterable immutable")
-	t.deepEqual( state2.toJS(), expected2, "Starts equal")
-	t.deepEqual( state1.saga.get(0).toJS(), expected1, "Has first saga entry")
-	t.deepEqual( state1.saga.get(1).toJS(), expected2, "Has first saga entry")
+	saga2.putState( state2)
+	t.ok( saga2.saga, "saga2 has a saga")
+	t.ok( Immutable.Iterable.isIterable(saga2.saga), "saga2 aga is iterable immutable")
+	t.equal( saga2.saga.size, 2, "saga2 has two elements")
+	t.deepEqual( saga2.state.toJS(), expected2, "Starts equal")
+	t.deepEqual( saga2.saga.get(0).toJS(), expected2, "Has first saga entry")
+	t.deepEqual( saga2.saga.get(1).toJS(), expected1, "Has first saga entry")
+	t.equal( saga1.saga.size, 1, "Original saga correct size")
 
 	t.end()
 })
--- sagaware.js
+++ sagaware.js
@@ -2,31 +2,42 @@ var
   Immutable= require( "immutable")
 
 function Sagaware( state, after){
-	if(typeof state !== "object"){
-		throw new Error("Invalid starting state")
+	if( this=== global|| !this){
+		return new Sagaware( state, after)
 	}
-	if( !Immutable.Iterable.isIterable(state)){
-		state= Immutable.fromJS(state)
+	if( state== undefined){
+		state= {}
+	}
+	if( typeof state !== "object"){
+		throw new Error("Invalid starting state "+ typeof state)
+	}
+	if( !Immutable.Iterable.isIterable( state)){
+		state= Immutable.fromJS( state)
 	}
 
-	var saga= Immutable.List.of()
-	function getSaga(){
-		return saga
-	}
-	function putState( state){
-		saga= saga.push( state)
-		Object.defineProperty( state, "saga", {
-			get: getSaga
-		})
-		Object.defineProperty( state, "putState", {
-			value: putState
-		})
-		if(after){
-			after(saga)
+	this.saga= Immutable.List.of( state)
+	Object.defineProperties(this, {
+		state: {
+			get: ()=> this.saga.get( 0)
+		},
+		clone: {
+			value: ()=> {
+				var s= Sagaware()
+				s.saga= this.saga
+				return s
+			}
+		},
+		putState: {
+			value: newState=> {
+				this.saga= this.saga.unshift( newState)
+				if (after){
+					after( this.saga)
+				}
+				return this
+			}
 		}
-		return state
-	}
-	return putState(state)
+	})
+	return this
 }
 
 module.exports= Sagaware
